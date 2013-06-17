308dc62d742212d165e114ebb8d0ada9cf0c14e7
scox <scox@pervasive.com>
2013-06-17 11:02:02 -0500

    Improvements for cygwin use

--- src/main/scripts/big-brother.bat
+++ src/main/scripts/big-brother.bat
@@ -1,2 +1,7 @@
 @ECHO OFF
-java -cp "%~dp0/../lib/*;" org.beeherd.metrics.BigBrotherApp %*
+
+set APP_HOME=%~dp0..
+
+java -Dscallop.app.config=%APP_HOME%/conf/app.properties -Dlog4j.configuration=file:/%APP_HOME%/conf/log4j.properties -cp "%APP_HOME%/lib/*;" org.beeherd.metrics.BigBrotherApp %*
+
+set APP_HOME=
